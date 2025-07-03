b21cb6f61591a3990303d86a94cb367c9692575a
jooszebi26 <43747192+jooszebi26@users.noreply.github.com>
2025-07-03 17:37:09 +0200

    initial commit

---
+++ movie-app-live-ready-to-present/movie-app-live-ready-to-present/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie-app-live-ready-to-present
+//
+//  Created by Szebasztian Joo on 2025. 07. 03..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie-app-live-ready-to-present/movie-app-live-ready-to-present/movie_app_live_ready_to_presentApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_app_live_ready_to_presentApp.swift
+//  movie-app-live-ready-to-present
+//
+//  Created by Szebasztian Joo on 2025. 07. 03..
+//
+
+import SwiftUI
+
+@main
+struct movie_app_live_ready_to_presentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
