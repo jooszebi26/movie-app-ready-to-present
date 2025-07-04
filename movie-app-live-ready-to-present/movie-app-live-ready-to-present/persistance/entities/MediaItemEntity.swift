433174dd8740917fc1955bf6803c9d3445f47174
jooszebi26 <43747192+jooszebi26@users.noreply.github.com>
2025-07-04 13:53:08 +0200

    offline data storage added

---
+++ movie-app-live-ready-to-present/movie-app-live-ready-to-present/persistance/entities/MediaItemEntity.swift
@@ -0,0 +1,46 @@
+
+//
+//  MediaItemEntity.swift
+//  movie-app-live
+//
+//  Created by Zsolt Pete on 2025. 05. 15..
+//
+
+import RealmSwift
+import Foundation
+
+class MediaItemEntity: Object {
+    @Persisted(primaryKey: true) var id: Int
+    @Persisted var title: String
+    @Persisted var year: String
+    @Persisted var duration: String
+    @Persisted var imageUrlString: String?
+    @Persisted var rating: Double
+    @Persisted var voteCount: Int
+}
+
+extension MediaItemEntity {
+    var toDomain: MediaItem {
+        MediaItem(
+            id: id,
+            title: title,
+            year: year,
+            duration: duration,
+            imageUrl: imageUrlString.flatMap(URL.init(string:)),
+            rating: rating,
+            voteCount: voteCount
+        )
+    }
+
+   convenience init(from domain: MediaItem) {
+        self.init()
+        self.id = domain.id
+        self.title = domain.title
+        self.year = domain.year
+        self.duration = domain.duration
+        self.imageUrlString = domain.imageUrl?.absoluteString
+        self.rating = domain.rating
+        self.voteCount = domain.voteCount
+    }
+}
+
